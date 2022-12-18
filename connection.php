201eac7465bfce897182e860abcc7681f3ce3375
SoumyarupChattejeeCSE <97448779+SoumyarupChattejeeCSE@users.noreply.github.com>
2022-12-18 08:11:37 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -9,6 +9,6 @@ $conn=mysqli_connect($servername,$username,$password,$dbname);
 if($conn){
 }
 else{
-	die("Connection failed because".mysqli_connect_error());
+	die("Connection to the database failed because".mysqli_connect_error());
 }
 ?>
